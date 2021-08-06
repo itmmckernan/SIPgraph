6ac606b2389e1bb594b84da89e2a98601ef317b0
itmmckernan <itmmckernan@gmail.com>
2021-08-06 11:17:17 -0700

    formatting

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ spreadsheet = spreadsheet.dropna(how='any', axis=0)
 
 fig = go.Figure()
 
+
 for index, row in spreadsheet.iterrows():
 
     print(row)
     fig.add_trace(go.Box(y=[row[1], row[2]], name=row[0]))
+
+
+fig.update_layout(
+    title="Comparison of Band Energies of Hole Blocking Layer Candidates",
+    xaxis_title="Candidate Materials",
+    yaxis_title="Band Energies",
+    legend_title="Candidate Materials",
+
+)
 
 fig.show()
 fig.write_html('index.html')
