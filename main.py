50b55a07565bcd7778aa5de3d6a9fc5de690f1f8
itmmckernan <itmmckernan@gmail.com>
2021-08-09 23:16:40 -0700

    change font size

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import plotly.graph_objects as go
 import pandas as pd
 
 spreadsheet = pd.read_csv('sheet.csv')
-spreadsheet = spreadsheet.drop(['Status','Link','Notes'], axis=1,)
+spreadsheet = spreadsheet.drop(['Status', 'Link', 'Notes'], axis=1, )
 spreadsheet = spreadsheet.dropna(how='any', axis=0)
 
 fig = go.Figure()
 
-
 for index, row in spreadsheet.iterrows():
-
     print(row)
     fig.add_trace(go.Box(y=[row[1], row[2]], name=row[0]))
-
 
 fig.update_layout(
     title="Comparison of Band Energies of Hole Blocking Layer Candidates",
     xaxis_title="Candidate Materials",
     yaxis_title="Band Energies",
     legend_title="Candidate Materials",
-
+    font={
+        'size': 25,
+    }
 )
 
 fig.show()
